f1e8ad3786d2bb45c91c995b4832928218c288d0
David Campos Rodríguez <caeucheunchinchoochan@msn.com>
2016-02-14 22:14:27 +0100

    El botoncito de volver funciona ahora

--- app/src/main/java/com/example/android/yummi/PricesActivity.java
+++ app/src/main/java/com/example/android/yummi/PricesActivity.java
@@ -1,8 +1,11 @@
 package com.example.android.yummi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class PricesActivity extends AppCompatActivity {
 
@@ -34,5 +37,25 @@ public class PricesActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().add(
                 R.id.prices_container, pricesActivityFragment).commit();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        } else if (id == R.id.action_aboutUs) {
+            startActivity(new Intent(this, AboutUsActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
